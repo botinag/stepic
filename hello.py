b3cd65c6193c7235be8b61bf4a66c71f6c1e2e6e
BP <bundin.pavel@gmail.com>
2016-03-13 13:07:32 +0300

    gunicorn init

---
+++ hello.py
@@ -0,0 +1,6 @@
+def simplest_wsgi_app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = environ['QUERY_STRING'].replace('&', '\n')
+    start_response(status, headers)
+    return [body]
