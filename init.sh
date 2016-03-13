91eacd4183005b46fb8de018f605ccd0e9c13000
BP <bundin.pavel@gmail.com>
2016-03-13 20:12:50 +0300

    add django

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf //home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf //home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf //home/box/web/etc/django_gunicorn.conf /etc/gunicorn.d/django_test
 sudo /etc/init.d/gunicorn restart
