79032aa267a1de5c52ca039d7a90e0d9712591de
BP <bundin.pavel@gmail.com>
2016-03-13 20:15:55 +0300

    delete double slaches

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -sf //home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/webetc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf //home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf //home/box/web/etc/django_gunicorn.conf /etc/gunicorn.d/django_test
+sudo ln -sf /home/box/webetc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/webetc/django_gunicorn.conf /etc/gunicorn.d/django_test
 sudo /etc/init.d/gunicorn restart
