3d559eeba5a01bfb75bf3b9a4b3a055b6ece752e
BP <bundin.pavel@gmail.com>
2016-03-17 22:17:50 +0300

    default value for rating in Question

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     title = models.CharField(max_length=55)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, related_name='author_set')
     likes = models.ManyToManyField(User, related_name='likes_set')
 
