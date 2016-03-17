bc97e6fefa1b8ba1b7e5fa3af3c6a4bf9a799532
BP <bundin.pavel@gmail.com>
2016-03-17 21:47:31 +0300

    templates & views

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-from qa.views import test
+from qa.views import test, new_questions, popular_questions, question_detail
 
 
 urlpatterns = patterns('',
@@ -12,10 +12,11 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^new/$', test, name='new'),
-    url(r'^popular/$', test, name='popular'),
+    url(r'^popular/$', popular_questions, name='popular'),
     url(r'^ask/', test, name='ask'),
-    url(r'^question/(?P<question_id>[0-9]+)/$', test, name='question_detail'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', question_detail, 
+        name='question_detail'),
     url(r'^signup/$', test, name='signup'),
     url(r'^login/$', test, name='login'),
-    url(r'^$', test, name='index'),
+    url(r'^$', new_questions, name='index'),
 )
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from qa.models import Question
 
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def new_questions(request):
+    limit = 10
+    page = request.GET.get('page')
+    paginator = Paginator(Question.objects.all().order_by('-pk'), limit)
+    try:
+        questions = paginator.page(page)
+    except PageNotAnInteger:
+        questions = paginator.page(1)
+    except EmptyPage:
+        questions = paginator.page(paginator.num_pages)
+    return render_to_response(
+        'qa/questions_list.html', {'questions': questions})
+
+
+def popular_questions(request):
+    limit = 10
+    page = request.GET.get('page')
+    paginator = Paginator(Question.objects.all().order_by('-rating'), limit)
+    try:
+        questions = paginator.page(page)
+    except PageNotAnInteger:
+        questions = paginator.page(1)
+    except EmptyPage:
+        questions = paginator.page(paginator.num_pages)
+    return render_to_response(
+        'qa/questions_list.html', {'questions': questions})
+
+
+def question_detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render_to_response(
+        'qa/question_detail.html', {'question': question})
